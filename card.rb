7e4cc06e676a16bfaf4595459501bd4c368e4e69
utgak <72766681+utgak@users.noreply.github.com>
2020-12-15 15:35:29 +0300

    Update card.rb

--- card.rb
+++ card.rb
@@ -3,7 +3,7 @@ class Card
   class << self; attr_reader :suits, :values end
 
   @suits = %w[♡ ♢ ♤ ♧]
-  @values = %w[1 2 3 4 5 6 7 8 9 10 J Q K A]
+  @values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
   def initialize(suit,value)
     @suit = suit
